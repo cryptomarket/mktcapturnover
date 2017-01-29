67a4806eef8fbbc023da81d55737793dc4a85edf
Stephen <sgornick@digicoast.com>
2017-01-29 15:06:13 -0800

    Add initial files, index.html, static subdirectory, and README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# mktcapturnover
+Cryptocurrency Daily Turnover Rate (@ Exchanges)
+
+## Deployment
+Clone or fork this repo to your own web server.
+
+## Example
+[cryptomarket.github.io/mktcapturnover](https://cryptomarket.github.io/mktcapturnover/)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ static/js/tickerchart.js
@@ -0,0 +1,56 @@
+var CORSURLPrefix;
+
+$(window).load(function() {
+    if ($('.se-pre-load').is(':visible')) {
+        $('.panel').hide();
+    }
+});
+
+$(function() {
+    var url;
+    CORSURLPrefix = 'https://cors-anywhere.herokuapp.com/';
+    url = 'https://api.coinmarketcap.com/v1/ticker?limit=100';
+    $.getJSON(CORSURLPrefix + url, handleGetJSONResponseTickerAPI);
+});
+
+function handleGetJSONResponseTickerAPI(data) {
+    var traceData, mkt_cap_threshold, day_vol_threshold;
+    mkt_cap_threshold = Math.pow(10, 6);  // Only want mkt cap >= $1M
+    day_vol_threshold = 5000;  // Only want trading vol >= $5K
+    traceData = {
+        'type': 'scatter', 'mode': 'markers', 'hoverinfo': 'text',
+        'x': [], 'y': [], 'text': [], 'marker': {'size': []}
+    };
+    $.each(data, function (index, value) {
+        if (value['market_cap_usd'] >= mkt_cap_threshold && 
+            value['24h_volume_usd'] >= day_vol_threshold) {        
+            traceData['x'].push(value['market_cap_usd']);
+            traceData['y'].push((value['24h_volume_usd'] / value['market_cap_usd']) * 100);
+            traceData['text'].push(value['name']);
+            traceData['marker']['size'].push(Math.log(value['24h_volume_usd'] / 10)); // Why / 10? To better show relative size.
+        };
+    });
+    $('.se-pre-load').hide();
+    $('.panel').delay(1500).show();
+    initiateChart([traceData]);
+}
+
+// Chart
+function initiateChart(data) {
+    var layout;
+    layout = {
+        title: 'Cryptocurrency Daily Turnover Rate (@ Exchanges)',
+        xaxis: {
+            'title': 'Market Cap',
+            'type': 'log'
+        },
+        yaxis: {
+            'title': '24H Turnover Ratio',
+            'type': 'log',
+            'ticksuffix': '%',
+            'dtick': 0.5
+        },
+        hovermode: 'closest'
+    };
+    Plotly.newPlot('mainChart', data, layout);
+}
