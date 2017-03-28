c746eb95aec94b220529534afc0401174947e8b9
Stephen <sgornick@digicoast.com>
2017-03-28 00:47:15 -0700

    Add subtitle with date.  Add 24h & 7d percentages to hovertext.

--- static/js/tickerchart.js
+++ static/js/tickerchart.js
@@ -12,7 +12,7 @@ $(function() {
 });
 
 function handleGetJSONResponseTickerAPI(data) {
-    var traceData, day_vol_threshold, colors;
+    var traceData, day_vol_threshold, colors, report_date;
     colors = ['rgb(204, 0, 0)', 'rgb(38, 77, 0)', 'rgb(38, 77, 0)']
     day_vol_threshold = 5000;  // Only want trading vol >= $5K
     traceData = {
@@ -23,21 +23,41 @@ function handleGetJSONResponseTickerAPI(data) {
         if (value['24h_volume_usd'] >= day_vol_threshold) {
             traceData['x'].push((value['24h_volume_usd'] / value['market_cap_usd']) * 100);
             traceData['y'].push(value['market_cap_usd']);
-            traceData['text'].push(value['name']);
+            traceData['text'].push(
+                value['name'] + '<br />24 Hour: ' + value['percent_change_24h'] +
+                '%<br />7 Day: ' + value['percent_change_7d'] + '%');
             traceData['marker']['size'].push(Math.log(value['24h_volume_usd']));
             traceData['marker']['color'].push(colors[Math.sign(value['percent_change_24h']) + 1]);
-        };
+        }
+        if (value['id'] === 'bitcoin') {
+            report_datetime = new Date(parseInt(value['last_updated']));
+        }
     });
     $('.se-pre-load').hide();
     $('.panel').delay(1500).show();
-    initiateChart([traceData]);
+    initiateChart([traceData], report_datetime);
+}
+
+function formatDateTimeUTC(date) {
+    var day, monthIndex, year, hours, minutes, seconds, monthNames;
+    monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December'
+    ];
+    day = date.getUTCDate();
+    monthIndex = date.getUTCMonth();
+    year = date.getUTCFullYear();
+    hours = date.getUTCHours();
+    minutes = date.getUTCMinutes();
+    seconds = date.getUTCSeconds();
+    return (monthNames[monthIndex].slice(0, 3) + ' ' + day + ', ' +  year + ' ' + ("0" + hours).slice(-2) + ':' + ("0" + minutes).slice(-2) + ':' + ("0" + seconds).slice(-2) + ' UTC');
 }
 
 // Chart
-function initiateChart(data) {
+function initiateChart(data, report_datetime) {
     var layout;
     layout = {
-        title: 'Cryptocurrency Daily Turnover Rate (@ Exchanges)',
+        title: 'Cryptocurrency Daily Turnover Rate (@ Exchanges)<br />As-of ' + formatDateTimeUTC(report_datetime),
         xaxis: {
             'title': '24H Turnover Ratio',
             'type': 'log',
@@ -49,6 +69,7 @@ function initiateChart(data) {
             'type': 'log'
         },
         hovermode: 'closest'
+
     };
     Plotly.newPlot('mainChart', data, layout);
 }
